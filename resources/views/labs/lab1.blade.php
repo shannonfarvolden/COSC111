e1804a634cd16e23f7a5c2a93a4ec85af57044af
Shannon Farvolden <shannonfarvolden@hotmail.com>
2017-01-04 13:24:42 -0800

    changes to lab 1

--- resources/views/labs/lab1.blade.php
+++ resources/views/labs/lab1.blade.php
@@ -186,24 +186,22 @@ My age is 216 in dog years!
         </div>
         <div class="panel-body">
             <p>
-                If you have your laptop with you and you plan on coding with it, you
-                should install Eclipse and then install Java JDK 8.
+                If you have your laptop with you and you plan on coding with it, you should install Eclipse.
+                Depending on your setup, you may also need to install Java JDK 8.
 
             <p>
                 To download Eclipse, go to <a href=http://www.eclipse.org/downloads/>the
-                    official Eclipse website</a> and select "Eclipse IDE for Java
-                Developers". From there, choose the operating system that matches your
-                machine and follow the instructions.
-
+                    official Eclipse website</a> and select "Get Eclipse Neon". From there, make
+                sure the operating system matches your machine and click on download. When you run the Eclipse
+                installer, it will open up a list of options for you to choose. Select "Eclipse IDE for Java Developers"
+                and then follow the instructions.
             <p>
                 To download JDK 8, go to <a
                         href=http://www.oracle.com/technetwork/java/javase/downloads/>the
                     official Oracle website</a> and select JDK 8 (or something similar --
-                currently, the site says "JDK 8u65"). Once you click on it, it will
-                bring you to a page with the appropriate options. Accept the license
-                agreement and choose the operating system that matches your machine.
-                Then follow the installation instructions.
-
+                currently, the site says "JDK 8u111"). Once you click on it, it will bring you to a page with the
+                appropriate options. Accept the license agreement and choose the operating system that matches your
+                machine. Then follow the installation instructions.
         </div>
     </div>
 @endsection
