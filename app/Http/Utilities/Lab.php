2411bf80597ba9a408c07e2fc08da3e3964a948c
Shannon Farvolden <shannonfarvolden@hotmail.com>
2017-01-05 00:46:30 -0800

    change one more view labs

--- app/Http/Utilities/Lab.php
+++ app/Http/Utilities/Lab.php
@@ -4,16 +4,20 @@ namespace App\Http\Utilities;
 
 class Lab
 {
-    // lab code => name
+
     protected static $labs = [
-        'L2A'=>'L2A',
-        'L2B'=>'L2B',
-        'L2C'=>'L2C'
+        'L2A' => 'L2A',
+        'L2B' => 'L2B',
+        'L2C' => 'L2C'
     ];
 
-
-
-public static function all(){
-    return static::$labs;
-}
+    /**
+     * Get all labs.
+     *
+     * @return array
+     */
+    public static function all()
+    {
+        return static::$labs;
+    }
 }
--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     {!! Form::label('lab', 'Lab Section') !!}
-    {!! Form::select('lab', ['L01'=>'L01', 'L02'=>'L02', 'L03'=>'L03'], old('lab'), ['class' => 'form-control']) !!}
+    {!! Form::select('lab', App\Http\Utilities\Lab::all(), old('lab'), ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('email', 'Email') !!}
