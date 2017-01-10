9eac57475275bf4a0ecc5356fb6112c56870387a
Shannon Farvolden <shannonfarvolden@hotmail.com>
2017-01-10 10:43:36 -0800

    fix slide layout

--- resources/views/slideset/index.blade.php
+++ resources/views/slideset/index.blade.php
@@ -8,34 +8,36 @@
         <a href="{{ action('SlideSetsController@create') }}" class=" btn btn-primary margin-button"> Create Slide
             Set</a>
     @endif
-    @foreach($slidesets->groupBy('week') as $week)
 
-        <div class="page-header">
-            <h1>Week {{$week->first()->week}}</h1>
-        </div>
-        <div class="col-md-4">
-            <h3>Topics</h3>
-            @if(!$week->where('category','topic')->isEmpty())
-                @foreach($week->where('category','topic')->sortBy('created_at') as $slideset)
-                   @include('slideset.partials.panel', ['slideset'=>$slideset])
-                @endforeach
-            @endif
-        </div>
-        <div class="col-md-4">
-            @if(!$week->where('category','summary')->isEmpty())
-                <h3>Summary</h3>
-                @foreach($week->where('category','summary')->sortBy('created_at') as $slideset)
-                    @include('slideset.partials.panel', ['slideset'=>$slideset])
-                @endforeach
-            @endif
-        </div>
-        <div class="col-md-4">
-            @if(!$week->where('category','exercise')->isEmpty())
-                <h3>Exercises</h3>
-                @foreach($week->where('category','exercise')->sortBy('created_at') as $slideset)
-                    @include('slideset.partials.panel', ['slideset'=>$slideset])
-                @endforeach
-            @endif
+    @foreach($slidesets->groupBy('week') as $week)
+        <div class="row">
+            <div class="page-header">
+                <h1>Week {{$week->first()->week}}</h1>
+            </div>
+            <div class="col-md-4">
+                <h3>Topics</h3>
+                @if(!$week->where('category','topic')->isEmpty())
+                    @foreach($week->where('category','topic')->sortBy('created_at') as $slideset)
+                        @include('slideset.partials.panel', ['slideset'=>$slideset])
+                    @endforeach
+                @endif
+            </div>
+            <div class="col-md-4">
+                @if(!$week->where('category','summary')->isEmpty())
+                    <h3>Summary</h3>
+                    @foreach($week->where('category','summary')->sortBy('created_at') as $slideset)
+                        @include('slideset.partials.panel', ['slideset'=>$slideset])
+                    @endforeach
+                @endif
+            </div>
+            <div class="col-md-4">
+                @if(!$week->where('category','exercise')->isEmpty())
+                    <h3>Exercises</h3>
+                    @foreach($week->where('category','exercise')->sortBy('created_at') as $slideset)
+                        @include('slideset.partials.panel', ['slideset'=>$slideset])
+                    @endforeach
+                @endif
+            </div>
         </div>
     @endforeach
 
