2ba1f49e75761206ffa00d6dea3ef9fd25d3f8ba
bohuie <bohuie@gmail.com>
2017-12-21 16:19:23 -0800

    blanked out all labs

--- resources/views/labs/lab3.blade.php
+++ resources/views/labs/lab3.blade.php
@@ -2,174 +2,36 @@
 
 @section('content')
     <div class="jumbotron">
-        <h2>Lab 3 [20 pts]</h2>
-        <p>Practicing Conditionals</p>
-        The purpose of this lab is to give you hands on practice with boolean
-        expressions and conditional statements.
-        If you need clarification with any of the steps below, ask your TA and/or
-        your neighbour.
+        <h2>Lab 3 [xx pts]</h2>
+        <p>TBA</p>
+        Brief overview
         <p></p>
         <b>What to Submit:</b>
         <ul>
-            <li>Show your TA you have completed Quiz 3 (nothing to submit)
-            <li>Typed answers for the boolean expressions exercise
-            <li><tt>FindNumDays.java</tt>
+            <li>Items to submit 
         </ul>
     </div>
 
     <div class="panel panel-default">
         <div class="panel-heading">
-            <h3 class="panel-title">1. Completed quiz mark [1 pt]</h3>
+            <h3 class="panel-title">1. Activity [xx pts]</h3>
         </div>
         <div class="panel-body">
-            At the beginning of the lab, show your TA for completing at least one
-            attempt for Quiz 3.
+            Description 
             <p>
-        </div>
-    </div>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">2. Boolean expressions [10 pts]</h3>
-        </div>
-        <div class="panel-body">
-            <p>
-                This exercise helps you get comfortable with evaluating boolean
-                expressions. For each of the following questions, indicate if the final
-                result is true or false. You should ideally do this by hand (with pen
-                and paper) because these types of questions will be similar to those
-                that appear on handwritten exams. However, for this lab, you can also
-                type the expression into Eclipse to help you verify your own answers.
-                Make sure you understand why the expressions evaluate to that particular
-                outcome.
-
-            <p>
-                <b>[1 pt each]</b>
-                Indicate the resulting value of each of the following:
-
-            <ol>
-                <li><tt>!true</tt>
-                <li><tt>!!true</tt>
-                <li><tt>true || true && false</tt>
-                <li><tt>3 * 3 - 3 > 2 && 4 - 2 > 5 </tt>
-                <li><tt>3 * 3 - 3 > 2 || 4 - 2 > 5 </tt>
-                <li><tt>x > 0 || (x < 10 && y < 0) </tt>, assume x = -1 and y = 3
-                <li><tt>!(x > 0) ^ (y == 3) </tt>, assume x = -1 and y = 3
-                <li><tt>(x + y > 10) && (y < 5) </tt>, assume x = -1 and y = 3
-                <li><tt>!y > 3 </tt>, assume y = 3
-                <li><tt>(x + y < 10) || !(y < 5) </tt>, assume x = -1 and y = 3
-            </ol>
-
-            Note: one of the expressions above generates a syntax error and cannot
-            be evaluated. You must identify that expression and explain why an error
-            occurred to get full marks.
-
-
         </div>
     </div>
 
     <div class="panel panel-default">
         <div class="panel-heading">
-            <h3 class="panel-title">2. Find Number of Days [9 pts]</h3>
+            <h3 class="panel-title">2. Activity [xx pts]</h3>
         </div>
         <div class="panel-body">
+			Description
             <p>
-                <b>[1 pt]</b>
-                This exercise gives you practice using comparison operators and nested
-                if-statements.
-                First, create a program called <tt>FindNumDays</tt>.
-
-            <p>
-                <b>[1 pt]</b>
-                Prompt the user to enter the month (as an integer from 1 to 12) and year
-                (as an integer with 4 digits).
-
-            <p>
-                <b>[1 pt]</b>
-                Be sure to check that the month and year entered are within the valid ranges.
-
-            <p>
-                <b>[1 pt]</b>
-                If the entered month and year are valid, the program may continue.
-                Otherwise, print out an error message to the user and the program will
-                end.
-
-            <p>
-                <b>[1 pt]</b>
-                Check if the year entered by the user is a leap year. Keep track of this
-                result in a boolean variable. (Recall the definition of a leap year
-                reviewed in the slides and also in the textbook.)
-
-            <p>
-                <b>[1 pt]</b>
-                A month has 30 days in April, June, September, and November, and 31 days
-                in all the other months aside from February. In February, a month has 29
-                days in a leap year, and only 28 days otherwise. Write the neceesary
-                if-statements to determine the number of days there are in the month
-                entered by the user.
-
-            <p>
-                <b>[1 pt]</b>
-                Display the number of days back to the user.
-
-            <p>
-                <b>[1 pt]</b>
-                Note that when you display the information, the n'th month will need to
-                be printed based on month, so that you use "1st" for January, "2nd" for
-                February, "3rd" for March, and "nth" for the other months. See sample
-                output below for clarification.
-
-            <p>
-                Sample output:
-            <pre>
-Enter a month (e.g., 1 for January):
-0
-Enter a year (e.g., 2012):
-1234
-Invalid input - please restart the program
-      </pre>
-            <p>
-                Sample output:
-            <pre>
-Enter a month (e.g., 1 for January):
-3
-Enter a year (e.g., 2012):
-123
-Invalid input - please restart the program
-      </pre>
-            <p>
-                Sample output:
-            <pre>
-Enter a month (e.g., 1 for January):
-6
-Enter a year (e.g., 2012):
-2014
-There are 30 days for the 6th month of year 2014
-      </pre>
-            <p>
-                Sample output:
-            <pre>
-Enter a month (e.g., 1 for January):
-2
-Enter a year (e.g., 2012):
-2012
-There are 29 days for the 2nd month of year 2012
-      </pre>
-            <p>
-                Sample output:
-            <pre>
-Enter a month (e.g., 1 for January):
-2
-Enter a year (e.g., 2012):
-2013
-There are 28 days for the 2nd month of year 2013
-      </pre>
-
-            <p>
-                <b>[1 pt]</b> Lastly, be sure to write comments above your class to
-                indicate the author of this file (you), acknowledgements for any
-                external help you got, and what the purpose of this program is.
         </div>
     </div>
+
 @endsection
 @section('footer')
     {{--Sends pageview google anaytics--}}
--- resources/views/labs/lab7.blade.php
+++ resources/views/labs/lab7.blade.php
@@ -2,156 +2,35 @@
 
 @section('content')
     <div class="jumbotron">
-    <h2>Lab 7 [24 pts]</h2>
-    <p>Working with Arrays</p>
-    The purpose of this lab is to give you hands on practice with arrays, from
-    declaring them, initializing them, passing them as arguments, as well as
-    reading from and writing to them.
-    If you need clarification with any of the steps below, ask your TA and/or
-    your neighbour.
-    <p> </p>
-    <b>What to Submit:</b>
-    <ul>
-      <li><tt>Part1.txt</tt>
-      <li><tt>ArrayHistogram.java</tt>
-    </ul>
+        <h2>Lab 7 [xx pts]</h2>
+        <p>TBA</p>
+        Brief overview
+        <p></p>
+        <b>What to Submit:</b>
+        <ul>
+            <li>Items to submit 
+        </ul>
     </div>
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">1. Short Exercises [10 pts]</h3>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">1. Activity [xx pts]</h3>
+        </div>
+        <div class="panel-body">
+            Description 
+            <p>
+        </div>
     </div>
-    <div class="panel-body">
-      <p>
-      This exercise gets you to do a series of short exercises pertaining to
-      arrays. 
-      The answers for this part of the lab will need to be typed up and saved
-      into a text document called <tt>Part1.txt</tt>. 
-      
-      <ol>
-      <li><b>[1 pt]</b>
-      Declare an array that stores 100 exam scores.
-      <li><b>[1 pt]</b>
-      Declare an array that stores 500 student names.
-      <li><b>[2pts: 1 pt for array declaration, 1 pt for using
-        <tt>random</tt> properly]</b>
-      Declare an array that stores N characters, where N can be anything
-      between 50 and 350 (inclusive).
-      <li><b>[1 pt]</b>
-      Given an array called <tt>num</tt>, assign its second element to -5.
-      <li><b>[1 pt]</b>
-      Given an array called <tt>num</tt>, assign its last element to -5.
-      <li><b>[4 pts: 1 pt method header and return statement, 1 pt for loop, 1 pt for correct index, 1 pt
-        for break]</b>
-      Create a method that takes an array of doubles called <tt>values</tt> as
-      input, and returns the index of the first element whose number is
-      greater than 5. If no such element exists, return -1.
-      </ol>
 
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">2. Activity [xx pts]</h3>
+        </div>
+        <div class="panel-body">
+			Description
+            <p>
+        </div>
     </div>
-  </div>
-
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">2. Visualizing a Histogram [14 pts]</h3>
-    </div>
-    <div class="panel-body">
-      <p>
-      <b>[1 pt]</b>
-      This exercise gets you to generate some random numbers and visualize
-      the frequency of those numbers as a histogram.
-      First, create a program called <tt>ArrayHistogram</tt>. 
-
-      <p>
-      <b>[1 pt]</b>
-      In your program, define an integer array called <tt>numbers</tt> that
-      holds 100 numbers. Separately, define an integer array called
-      <tt>frequencies</tt> that holds 10 numbers.
-      
-      <p>
-      <b>[2 pts]</b>
-      Create a method called <tt>initArray</tt> that initializes all the
-      elements in <tt>numbers</tt> to be a random integer between 0 and 9
-      (inclusive). 
-      Call this method inside <tt>main</tt>. You can print out the elements of
-      your array to ensure everything is working properly before moving on.
-      
-      <p>
-      <b>[2 pts]</b>
-      Remember that <tt>numbers</tt> is an array that stores a number between
-      0 to 9. What we want to do is count the number of times each of those
-      digits appear in the <tt>numbers</tt> array and store that in
-      <tt>frequencies</tt>.  For example, if the digit 0 shows up 12 times,
-      then the value of <tt>frequencies[0]</tt> is 12.
-      Create a method called <tt>createHistogram</tt> that loops through each
-      element in <tt>numbers</tt> and stores those occurrences in
-      <tt>frequencies</tt>.
-      
-      <p>
-      <b>[1 pt]</b>
-      Call <tt>createHistogram</tt> inside <tt>main</tt> and make sure
-      everything works properly.
-      
-      <p>
-      <b>[1 pt]</b>
-      Define a third method called <tt>prString</tt> that concatenates all the
-      information that needs to be printed into a single <tt>String</tt>
-      variable and returns that <tt>String</tt> as output.
-      
-      <p>
-      <b>[2 pts]</b>
-      The purpose of <tt>prString</tt> is two-folds. First, you will get it to
-      display all the values inside the array <tt>numbers</tt>. You wil need
-      to concatenate these numbers into a <tt>String</tt> variable. These
-      numbers should be comma separated, and should have 20 numbers on each
-      line. The last number in the array will not have a following comma. See
-      sample output below for an example.
-      
-      <p>
-      <b>[2 pts]</b>
-      Second, <tt>prString</tt> will concatenate a series of stars ("*") for
-      the <tt>frequencies</tt> array so that the output looks like a sideways
-      bargraph. For example, if <tt>frequencies[5]</tt> is 6, then the line
-      corresponding to that is "5: ******". See sample output below for an
-      example. Call this method inside <tt>main</tt> when you are done.
-      
-      <p>
-      <b>[1 pt]</b>
-      Note that since <tt>prString</tt> returns a single <tt>String</tt> that
-      stores all the information to be printed together into one variable,
-      there will be <b>no print statements</b> inside this method.
-      In fact, in the entire program, all you need is one <tt>println</tt>
-      statement inside <tt>main</tt> to have it work.
-      
-      <p>
-      Sample output:
-      <pre>
-The randomly generated numbers are:
-6, 8, 5, 3, 2, 5, 7, 5, 6, 9, 3, 0, 6, 2, 6, 3, 4, 3, 8, 6, 
-3, 3, 6, 0, 6, 5, 6, 1, 7, 3, 3, 6, 4, 1, 6, 1, 0, 7, 7, 2, 
-2, 6, 5, 1, 1, 8, 4, 4, 6, 4, 4, 1, 6, 5, 3, 6, 8, 5, 9, 4, 
-5, 9, 1, 9, 8, 6, 0, 7, 4, 3, 2, 5, 2, 6, 1, 8, 5, 8, 4, 3, 
-3, 6, 8, 4, 2, 6, 9, 7, 6, 3, 7, 7, 2, 8, 5, 0, 9, 9, 2, 4
-
-The histogram looks like:
-0: *****
-1: ********
-2: *********
-3: *************
-4: ***********
-5: ***********
-6: *******************
-7: ********
-8: *********
-9: *******
-      </pre>
-
-      <p>
-      <b>[1 pt]</b> Lastly, be sure to write comments above your class to
-      indicate the author of this file (you), acknowledgements for any
-      external help you got, and what the purpose of this program is.
-    </div>
-  </div>
 
 @endsection
 @section('footer')
